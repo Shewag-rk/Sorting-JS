cc22a31286890b8a74e4be7fe3500f9a8e1a5108
shewag-rk <shewagsurya02@gmail.com>
2025-01-16 10:54:34 +0530

    JS

--- sort.js
+++ sort.js
@@ -1,31 +1,31 @@
-// 1. Question
+1.
 
-// var arr=[29 , -10 , 30 , 10 , 1 , 54 , 3];
-// var temp="";
-// for(var outer=0; outer<arr.length; ++outer){
-//         for(var inner=outer+1; inner<arr.length; ++inner)
-//         if(arr[outer]>arr[inner]){
-//             temp=arr[outer]
-//             arr[outer]=arr[inner];
-//             arr[inner]=temp;
-//         }
-// }
-// console.log(arr);
+var arr=[29 , -10 , 30 , 10 , 1 , 54 , 3];
+var temp="";
+for(var outer=0; outer<arr.length; ++outer){
+        for(var inner=outer+1; inner<arr.length; ++inner)
+        if(arr[outer]>arr[inner]){
+            temp=arr[outer]
+            arr[outer]=arr[inner];
+            arr[inner]=temp;
+        }
+}
+console.log(arr);
     
 
 
-// 2. Question
+2.
 
-// var arr=[1,2,3,4,5,67,8];
-// var temp="";
-// for(var outer=0; outer<arr.length; ++outer){
-//         for(var inner=outer+1; inner<arr.length; ++inner)
-//         if(arr[outer]<arr[inner] || arr[outer]<arr[inner]){
-//             temp=arr[outer]
-//             arr[outer]=arr[inner];
-//             arr[inner]=temp;
-//         }else{
-//             console.log("Invaild input")
-//         }
-// }
-// console.log(arr);
+var arr=[1,2,3,4,5,67,8];
+var temp="";
+for(var outer=0; outer<arr.length; ++outer){
+        for(var inner=outer+1; inner<arr.length; ++inner)
+        if(arr[outer]<arr[inner] || arr[outer]<arr[inner]){
+            temp=arr[outer]
+            arr[outer]=arr[inner];
+            arr[inner]=temp;
+        }else{
+            console.log("Invaild input")
+        }
+}
+console.log(arr);
